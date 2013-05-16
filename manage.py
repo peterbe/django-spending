5100919bd818fae4da4771bff82ec5e1305eda3b
Peter Bengtsson <mail@peterbe.com>
2013-05-15 20:47:06 -0700

    better wsgi file

--- manage.py
+++ manage.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+import site
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spending.settings")
 
-    import site
+    prev_sys_path = list(sys.path)
     site.addsitedir('vendor-local/lib/python')
-    from django.core.management import execute_from_command_line
 
+    new_sys_path = []
+    for item in list(sys.path):
+        if item not in prev_sys_path:
+            new_sys_path.append(item)
+            sys.path.remove(item)
+    sys.path[:0] = new_sys_path
+
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
---
+++ wsgi.py
@@ -0,0 +1,40 @@
+"""
+WSGI config for spending project.
+
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
+
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
+import os
+import sys
+import site
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spending.settings")
+
+prev_sys_path = list(sys.path)
+site.addsitedir('vendor-local/lib/python')
+
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+import django
+print django.__file__
+print django.get_version()
